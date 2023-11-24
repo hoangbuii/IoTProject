af3d273a03b5effc2b7d1f05489bf80bc9322f5e
Hoàng Bùi <60215764+hoangbuii@users.noreply.github.com>
2023-11-24 18:06:26 +0700

    Add files via upload

---
+++ client/client.ino
@@ -0,0 +1,221 @@
+
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "nha";
+const char* password = "12345678";
+const char* mqtt_server = "test.mosquitto.org";
+
+#define DHTTYPE DHT11
+
+#define DHT_PIN D2
+#define SOIL_MOISTURE_PIN A0
+#define LUX_PIN D1
+#define AIR_PIN D0
+#define LIGHT1_PIN D3
+#define LIGHT2_PIN D4
+#define PUMP_PIN D5
+
+DHT dht(DHT_PIN, DHTTYPE);
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+float temp = 0;
+float humid = 0;
+int lux = 0;
+int soil = 0;
+int light1_st = 0;
+int light2_st = 0;
+int air_st = 0;
+int pump_st = 0;
+
+void setup_wifi() {
+  
+    delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void processData(char* topic, byte* payload) {
+  if (strcmp(topic, "light1") == 0) {
+    if((char)payload[0] == '1') {
+      digitalWrite(LIGHT1_PIN, HIGH);
+      light1_st = 1;
+    } else {
+      digitalWrite(LIGHT1_PIN, LOW);
+      light1_st = 0;
+    }
+    
+  } else if (strcmp(topic, "light2") == 0) {
+    if((char)payload[0] == '1') {
+      digitalWrite(LIGHT2_PIN, HIGH);
+      light2_st = 1;
+    } else {
+      digitalWrite(LIGHT2_PIN, LOW);
+      light2_st = 0;
+    }
+  } else if (strcmp(topic, "air") == 0) {
+    if((char)payload[0] == '1') {
+      digitalWrite(AIR_PIN, HIGH);
+      air_st = 1;
+    } else {
+      digitalWrite(AIR_PIN, LOW);
+      air_st = 0;
+    }
+  } else if (strcmp(topic, "pump") == 0) {
+    if((char)payload[0] == '1') {
+      digitalWrite(PUMP_PIN, HIGH);
+      pump_st = 1;
+    } else {
+      digitalWrite(PUMP_PIN, LOW);
+      pump_st = 0;
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  processData(topic, payload);
+  // if((char)payload[0] == '1') {
+  //   digitalWrite(LED, HIGH);
+  // } else {
+  //   digitalWrite(LED, LOW); 
+  // }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("temp", "hello IoT Gateway...");
+      client.publish("hum", "hello IoT Gateway...");
+      client.publish("lux", "hello IoT Gateway...");
+      client.publish("soilMoisture", "hello IoT Gateway...");
+      client.publish("light1-st", "hello IoT Gateway...");
+      client.publish("light2-st", "hello IoT Gateway...");
+      client.publish("air-st", "hello IoT Gateway...");
+      client.publish("pump-st", "hello IoT Gateway...");
+      // ... and resubscribe
+      client.subscribe("light1");
+      client.subscribe("light2");
+      client.subscribe("air");
+      client.subscribe("pump");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void sendData() {
+  temp = dht.readTemperature();
+  humid = dht.readHumidity();
+  lux = digitalRead(LUX_PIN);
+  int soilMoistureValue = analogRead(SOIL_MOISTURE_PIN);
+  soil = map(soilMoistureValue, 1023, 0, 0, 100);
+  char tmp[10];
+
+  dtostrf(temp, 4, 3, tmp);
+  snprintf (msg, MSG_BUFFER_SIZE, tmp);
+  client.publish("temp", msg);
+  dtostrf(humid, 4, 1, tmp);
+  snprintf (msg, MSG_BUFFER_SIZE, tmp);
+  client.publish("hum", msg);
+  dtostrf(lux, 1, 0, tmp);
+  snprintf (msg, MSG_BUFFER_SIZE, tmp);
+  client.publish("lux", msg);
+  dtostrf(soil, 4, 0, tmp);
+  snprintf (msg, MSG_BUFFER_SIZE, tmp);
+  client.publish("soilMoisture", msg);
+  
+  dtostrf(light1_st, 1, 0, tmp);
+  snprintf (msg, MSG_BUFFER_SIZE, tmp);
+  client.publish("light1-st", msg);
+  dtostrf(light2_st, 1, 0, tmp);
+  snprintf (msg, MSG_BUFFER_SIZE, tmp);
+  client.publish("light2-st", msg);
+  dtostrf(air_st, 1, 0, tmp);
+  snprintf (msg, MSG_BUFFER_SIZE, tmp);
+  client.publish("air-st", msg);
+  dtostrf(pump_st, 1, 0, tmp);
+  snprintf (msg, MSG_BUFFER_SIZE, tmp);
+  client.publish("pump-st", msg);
+
+
+
+
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  setup_wifi();
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(LUX_PIN, INPUT);
+  pinMode(AIR_PIN, OUTPUT);
+  pinMode(LIGHT1_PIN, OUTPUT);
+  pinMode(LIGHT2_PIN, OUTPUT);
+  pinMode(PUMP_PIN, OUTPUT);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    sendData();
+    // float temperature = dht.readTemperature();
+    // char tmp[10]; 
+    // dtostrf(temperature, 4, 3, tmp);
+    // snprintf (msg, MSG_BUFFER_SIZE, tmp);
+    // Serial.print("Publish message temperature: ");
+    // Serial.println(msg);
+    // client.publish("sensingData4", msg);
+
+  }
+
+
+}
